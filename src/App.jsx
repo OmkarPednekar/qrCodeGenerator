e2de9fc0ca008bfca80dff002fcfe225a0868602
omkar <omkar.pednekar@digiplusit.com>
2024-10-06 05:04:17 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,342 +1,128 @@
 import { useState } from "react";
 import "./App.css";
 import { exponentToInteger, integerToExponent } from "./logstables";
+import { fixedPos, whitePos } from "./positions";
 function App() {
   const [input, setInput] = useState();
   const [binary, setBinary] = useState();
   const [matrix, setMatrix] = useState([]);
-  const [mark, setMark] = useState([]);
   const [messagePloy, setMessagePloy] = useState([]);
-  // Initialize a 25x25 matrix (625 cells)
-  var qrMatrix = Array.from({ length: 25 }, () => Array(25).fill([0, "||"]));
-  var fixedPos = [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [0, 4],
-    [0, 5],
-    [0, 6],
-    [1, 0],
-    [2, 0],
-    [3, 0],
-    [4, 0],
-    [5, 0],
-    [6, 0],
-    [6, 1],
-    [6, 2],
-    [6, 3],
-    [6, 4],
-    [6, 5],
-    [6, 6],
-    [6, 6],
-    [6, 6],
-    [5, 6],
-    [4, 6],
-    [3, 6],
-    [2, 6],
-    [1, 6],
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [3, 2],
-    [3, 3],
-    [3, 4],
-    [4, 2],
-    [4, 3],
-    [4, 4],
-    [24, 0],
-    [24, 1],
-    [24, 2],
-    [24, 3],
-    [24, 4],
-    [24, 5],
-    [24, 6],
-    [23, 0],
-    [22, 0],
-    [21, 0],
-    [20, 0],
-    [19, 0],
-    [18, 0],
-    [18, 1],
-    [18, 2],
-    [18, 3],
-    [18, 4],
-    [18, 5],
-    [18, 6],
-    [22, 6],
-    [21, 6],
-    [20, 6],
-    [19, 6],
-    [23, 6],
-    [22, 2],
-    [22, 3],
-    [22, 4],
-    [21, 2],
-    [21, 3],
-    [21, 4],
-    [20, 2],
-    [20, 3],
-    [20, 4],
-    [0, 24],
-    [1, 24],
-    [2, 24],
-    [3, 24],
-    [4, 24],
-    [5, 24],
-    [6, 24],
-    [6, 23],
-    [6, 22],
-    [6, 21],
-    [6, 20],
-    [6, 19],
-    [6, 18],
-    [5, 18],
-    [4, 18],
-    [3, 18],
-    [2, 18],
-    [1, 18],
-    [0, 18],
-    [0, 19],
-    [0, 20],
-    [0, 21],
-    [0, 22],
-    [0, 23],
-    [2, 20],
-    [2, 21],
-    [2, 22],
-    [3, 20],
-    [3, 21],
-    [3, 22],
-    [4, 20],
-    [4, 21],
-    [4, 22],
-    [20, 20],
-    [20, 19],
-    [20, 18],
-    [20, 17],
-    [20, 16],
-    [19, 16],
-    [18, 16],
-    [17, 16],
-    [16, 16],
-    [16, 17],
-    [16, 18],
-    [16, 19],
-    [16, 20],
-    [17, 20],
-    [18, 20],
-    [19, 20],
-    [18, 18],
-    [8, 6],
-    [10, 6],
-    [12, 6],
-    [14, 6],
-    [16, 6],
-    [6, 8],
-    [6, 10],
-    [6, 12],
-    [6, 14],
-    [6, 16],
-    [17, 8],
-    [8, 0],
-    [8, 1],
-    [8, 2],
-    [8, 3],
-    [8, 8],
-    [4, 8],
-    [3, 8],
-    [2, 8],
-    [0, 8],
-    [24, 8],
-    [23, 8],
-    [22, 8],
-    [21, 8],
-    [8, 17],
-    [8, 20],
-    [8, 21],
-    [8, 22],
-    [8, 24],
-  ];
-  var whitePos = [
-    [1, 1],
-    [1, 2],
-    [1, 3],
-    [1, 4],
-    [1, 5],
-    [2, 5],
-    [3, 5],
-    [4, 5],
-    [5, 5],
-    [5, 4],
-    [5, 3],
-    [5, 2],
-    [5, 1],
-    [4, 1],
-    [3, 1],
-    [2, 1],
-    [7, 0],
-    [7, 1],
-    [7, 2],
-    [7, 3],
-    [7, 4],
-    [7, 5],
-    [7, 6],
-    [7, 7],
-    [6, 7],
-    [5, 7],
-    [4, 7],
-    [3, 7],
-    [2, 7],
-    [1, 7],
-    [0, 7],
-    [1, 19],
-    [1, 20],
-    [1, 21],
-    [1, 22],
-    [1, 23],
-    [2, 23],
-    [3, 23],
-    [4, 23],
-    [5, 23],
-    [5, 22],
-    [5, 21],
-    [5, 20],
-    [5, 19],
-    [4, 19],
-    [3, 19],
-    [2, 19],
-    [1, 19],
-    [0, 17],
-    [1, 17],
-    [2, 17],
-    [3, 17],
-    [4, 17],
-    [5, 17],
-    [6, 17],
-    [7, 17],
-    [7, 18],
-    [7, 19],
-    [7, 20],
-    [7, 21],
-    [7, 22],
-    [7, 23],
-    [7, 24],
-    [17, 17],
-    [17, 18],
-    [17, 19],
-    [18, 19],
-    [19, 19],
-    [19, 18],
-    [19, 17],
-    [18, 17],
-    [19, 1],
-    [19, 2],
-    [19, 3],
-    [19, 4],
-    [19, 5],
-    [20, 5],
-    [21, 5],
-    [22, 5],
-    [23, 5],
-    [23, 4],
-    [23, 3],
-    [23, 2],
-    [23, 1],
-    [22, 1],
-    [21, 1],
-    [20, 1],
-    [17, 0],
-    [17, 1],
-    [17, 2],
-    [17, 3],
-    [17, 4],
-    [17, 5],
-    [17, 6],
-    [17, 7],
-    [18, 7],
-    [19, 7],
-    [20, 7],
-    [21, 7],
-    [22, 7],
-    [23, 7],
-    [24, 7],
-    [9, 6],
-    [11, 6],
-    [13, 6],
-    [15, 6],
-    [6, 9],
-    [6, 11],
-    [6, 13],
-    [6, 15],
-
-    [8, 4],
-    [8, 5],
-    [8, 7],
-    [7, 8],
-    [5, 8],
-    [1, 8],
-    [20, 8],
-    [19, 8],
-    [18, 8],
-    [8, 18],
-    [8, 19],
-    [8, 23],
-  ];
+  var qrMatrix = Array.from({ length: 25 }, () => Array(25).fill([0, "X"]));
   const generateErrorCodes = (input) => {
-    let generatorPloyPowers = [0, 251, 67, 46, 61, 118, 70, 64, 94, 32, 45];
-    let generatorPloyCoeffPowers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-    let messageXPowers = [];
-    let paddedArray = input;
-    if (input.length < 10) {
-      let reqlength = input.length + 9;
-      for (var p = 0; p <= reqlength; ++p) {
-        messageXPowers.push(reqlength - p);
-      }
-      paddedArray = [...input, ...Array(reqlength - input.length + 1).fill(0)];
-      for (var g = 0; g < generatorPloyCoeffPowers.length; g++) {
-        generatorPloyCoeffPowers[g] =
-          generatorPloyCoeffPowers[g] + (input.length - 1);
-      }
-    } else {
-      for (var g = 0; g < generatorPloyCoeffPowers.length; g++) {
-        generatorPloyCoeffPowers[g] =
-          generatorPloyCoeffPowers[g] + (input.length - 1 - 10);
-      }
+    const generatorPolyPowers = [0, 251, 67, 46, 61, 118, 70, 64, 94, 32, 45];
+    const paddedArray = [...input]; // Clone input array
+
+    let codeArr = new Array(input.length + 10).fill(0);
+    for (let i = 0; i < input.length; i++) {
+      codeArr[i] = input[i];
     }
-    let codeArr = paddedArray;
-    let genPolyPow = generatorPloyPowers;
-    for (var x = 0; x < input.length; x++) {
-      // find coefficient of alpha
+    for (let x = 0; x < input.length; x++) {
+      let coefficientToIn = integerToExponent[codeArr[x]];
+      const currentGenPolyPowers = generatorPolyPowers.map(
+        (power) => power + coefficientToIn
+      );
 
-      let coefficientToIn = integerToExponent[codeArr[0]];
-      let intToCoeffi = [];
-      for (var i = 0; i < generatorPloyPowers.length; i++) {
-        let val =
-          [0, 251, 67, 46, 61, 118, 70, 64, 94, 32, 45][i] + coefficientToIn;
-
-        if (val > 255) {
-          val = val % 255;
+      const intToCoeffi = currentGenPolyPowers.map((val) => {
+        const normalized = val % 255;
+        return exponentToInteger[normalized];
+      });
+      for (let j = 0; j < intToCoeffi.length; j++) {
+        if (j + x < codeArr.length) {
+          codeArr[j + x] ^= intToCoeffi[j];
         }
-        genPolyPow[i] = val;
-
-        intToCoeffi.push(exponentToInteger[genPolyPow[i]]);
       }
-      let xorVal = [];
-      for (var j = 0; j < intToCoeffi.length; j++) {
-        xorVal.push(codeArr[j] ^ intToCoeffi[j]);
-      }
-      codeArr = xorVal.slice(1);
-
-      // console.log(genPolyPow);
-      //add that to generatorPloyPowers and %255 if value > 256
-      //find intergers of those values
-      //do a XOR between those intergers and paddedArray
-      //eliminate 0 term and this will be new message ploynomial array
-      //repeat
     }
-    return codeArr;
+    const errorCodes = codeArr.slice(input.length);
+    return errorCodes;
   };
+
+  const markBits = (data) => {
+    var col = 24; // Start at the rightmost column
+    var row = 24; // Start at the bottom row
+    let pointer = 0; // Points to the current bit in the data array
+    let direction = 0; // -1 for moving up, 1 for moving down
+    // Matrix to fill with data bits
+    for (var x = 0; x < fixedPos.length; x++) {
+      qrMatrix[fixedPos[x][0]][fixedPos[x][1]] = [1, "Y"];
+    }
+    for (var y = 0; y < whitePos.length; y++) {
+      qrMatrix[whitePos[y][0]][whitePos[y][1]] = [0, "Y"];
+    }
+    let pos = [];
+    while (pointer < data.length) {
+      // Place bit only if this spot is marked as 'X' (editable)
+      if (qrMatrix[row][col][1] == "X") {
+        // console.log(row, col, parseInt(data[pointer]));
+        pos.push([row, col]);
+        pointer++;
+      }
+
+      if (direction == 0) {
+        if (col % 2 == 0) {
+          col -= 1; // Move left when column is even
+        } else {
+          if (row != 0) {
+            col += 1;
+          }
+          // Move right
+          row -= 1; // Move up
+        }
+      } else {
+        if (col % 2 == 0) {
+          col -= 1; // Move left when column is even
+        } else {
+          if (row != 24) {
+            col += 1;
+          }
+
+          row += 1; // Move down
+        }
+      }
+
+      // Handle boundary conditions
+      if (row < 0 || row > 24) {
+        if (row < 0) {
+          row = 0; // Reset to top row if it goes out of bounds
+          direction = 1; // Switch direction to move downwards
+        }
+        if (row > 24) {
+          row = 24; // Reset to bottom row if it goes out of bounds
+          direction = 0; // Switch direction to move upwards
+        } // Correct column when moving left after hitting a boundary
+
+        col -= 1;
+        // This should now properly decrement and print
+      }
+    }
+
+    for (var i = 0; i < data.length; i++) {
+      let r = pos[i][0];
+      let c = pos[i][1];
+
+      qrMatrix[r][c] = data[i];
+    }
+
+    const numRows = qrMatrix.length; // Assuming qrMatrix is square
+    const numCols = qrMatrix[0].length; // Get the number of columns
+    const maskPattern = 3; // Mask every third column (you can adjust this)
+
+    for (let column = 0; column < numCols; column++) {
+      if (column % maskPattern === 0) {
+        // Apply mask condition
+        for (let row = 0; row < numRows; row++) {
+          if (qrMatrix[row][column] == 0 || qrMatrix[row][column] == 1) {
+            let curr = qrMatrix[row][column];
+            qrMatrix[row][column] = curr == 0 ? 1 : 0;
+          }
+          if (qrMatrix[row][column][1] === "X") {
+            qrMatrix[row][column] = qrMatrix[row][column][0] === 0 ? 1 : 0;
+          }
+        }
+      }
+    }
+    setMatrix(qrMatrix); // Update matrix with the filled data
+  };
+
   const getBinary = () => {
     let ascii = [];
     ascii.push("0100");
@@ -354,46 +140,42 @@ function App() {
       messagePolyarr.push(parseInt(binaryString.slice(a, a + 8), 2));
     }
     setMessagePloy(messagePolyarr);
-    setBinary(binaryString);
-    for (var x = 0; x < fixedPos.length; x++) {
-      qrMatrix[fixedPos[x][0]][fixedPos[x][1]] = [1, "Y"];
-    }
-    for (var y = 0; y < whitePos.length; y++) {
-      qrMatrix[whitePos[y][0]][whitePos[y][1]] = [0, "Y"];
-    }
+
     let errorCodeWords = generateErrorCodes(messagePolyarr);
     let errCodeArr = [];
     for (var i = 0; i < errorCodeWords.length; i++) {
       let binary = errorCodeWords[i].toString(2).padStart(8, "0");
       errCodeArr.push(binary);
     }
-    // let codeWords = [58, 64, 142, 238, 199, 51, 89, 207, 88, 213];
+    let finalCode = ascii.join("") + errCodeArr.join("");
+    setBinary(finalCode);
+    markBits(finalCode);
+  };
 
-    console.log(ascii, errCodeArr);
-    setMatrix(qrMatrix);
-  };
-  const markit = (row, col) => {
-    let tmp = mark;
-    tmp.push([row, col]);
-    setMark(tmp);
-    console.log(tmp);
-  };
   return (
     <>
-      <div>
+      <div className="divtext">
         <input
+          className="input"
           type="text"
           onChange={(e) => {
             setInput(e.target.value);
           }}
         ></input>
-        <button onClick={getBinary}>GENERATE QR</button>
+        <button className="button" onClick={getBinary}>
+          GENERATE QR
+        </button>
+        <br />
+        <br />
+        QR Version : 2 (21X21)
+        <br />
+        Error Correction Level : L (upto 7%)
         <br />
         MESSAGE : {binary}
         <br />
         LENGTH : {binary != undefined ? binary.length : ""}
         <br />
-        MESSAGE PLOYNOMIAL :{" "}
+        MESSAGE POLYNOMIAL :{" "}
         <div style={{ display: "flex" }}>
           {messagePloy.length > 0 ? (
             messagePloy.map((data, index) => {
@@ -422,7 +204,7 @@ function App() {
       {matrix.length < 1 ? (
         <p></p>
       ) : (
-        <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", width: "100%" }}>
           <div
             style={{
               width: "500px",
@@ -441,7 +223,8 @@ function App() {
                     className="divitem"
                     key={i}
                     style={{
-                      backgroundColor: val[0] == 1 ? "black" : "white",
+                      backgroundColor:
+                        val[0] == 1 ? "black" : val == 1 ? "black" : "white",
                       // border: "1px solid #dce1e8",
                     }}
                   ></div>
@@ -449,6 +232,7 @@ function App() {
               });
             })}
           </div>
+
           <div
             style={{
               width: "700px",
@@ -456,7 +240,7 @@ function App() {
               display: "grid",
               gridTemplateColumns: "repeat(25, 1fr)",
               gridTemplateRows: "repeat(25, 1fr)",
-              // border: "20px solid white",
+              border: "20px solid white",
               boxSizing: "border-box",
             }}
           >
@@ -465,28 +249,25 @@ function App() {
                 return (
                   <div
                     key={i}
-                    onClick={() => {
-                      markit(index, i);
-                    }}
                     className="divitem"
                     style={{
                       backgroundColor:
-                        val[1] == "Y" ? "lightgray" : "greenyellow",
-                      border:
-                        val[0] == 1 ? "1px solid black" : "1px solid #dce1e8",
+                        val[0] == 0 ? "white" : val == 0 ? "white" : "black",
+                      border: "1px solid black",
+                      //   val[0] == 1
+                      //     ? "1px solid black"
+                      //     : val == 1
+                      //     ? "1px solid black"
+                      //     : "1px solid #dce1e8",
                       color:
-                        val[1] == "Y"
-                          ? val[0] == 0
-                            ? "yellow"
-                            : "red"
-                          : "blue",
+                        val[0] == 0 ? "black" : val == 0 ? "black" : "white",
                       textAlign: "center",
 
                       // transform:
                       //   val[1] == "|" ? `rotate(${(360 % index) + 1}deg)` : "",
                     }}
                   >
-                    {val[0]}
+                    <strong>{val[1] === undefined ? val : val[0]}</strong>
                   </div>
                 );
               });
